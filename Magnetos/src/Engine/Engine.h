5e54a91da48de178a11f8549281394dec8e082aa
Lukas Feldgrill <64472839+lufeld@users.noreply.github.com>
2023-04-25 19:56:46 +0200

    OpenGL and GLFW integrated project

---
+++ Magnetos/src/Engine/Engine.h
@@ -0,0 +1,15 @@
+#ifndef MAGNETOS_ENGINE
+#define MAGNETOS_ENGINE
+
+#include "GLFW/glfw3.h"
+//#pragma comment(lib, "opengl32.lib")
+
+class Engine
+{
+public:
+
+private:
+
+};
+
+#endif // !MAGNETOS_ENGINE
